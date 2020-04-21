354897f2c44e142e2871c2a07706f45cfa1fb2c7
Kykung <59070071@it.kmitl.ac.th>
2020-04-21 15:16:35 +0700

    Add hybrid loss

--- losses.py
+++ losses.py
@@ -4,6 +4,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
+def hybrid_loss(target, input_values, gamma):
+    """Computes the focal loss"""
+
+    p = torch.exp(-input_values)
+    loss = (1 - p) ** gamma * input_values
+    final_loss = 0
+
+    for _class in torch.unique(target):
+        index = torch.where(condition = target == _class)
+        final_loss += torch.take(loss, index[0]).mean()
+
+    return final_loss
+
 def focal_loss(input_values, gamma):
     """Computes the focal loss"""
     p = torch.exp(-input_values)
@@ -19,6 +32,16 @@ class FocalLoss(nn.Module):
 
     def forward(self, input, target):
         return focal_loss(F.cross_entropy(input, target, reduction='none', weight=self.weight), self.gamma)
+
+class HybridLoss(nn.Module):
+    def __init__(self, weight=None, gamma=0.):
+        super(HybridLoss, self).__init__()
+        assert gamma >= 0
+        self.gamma = gamma
+        self.weight = weight
+    
+    def forward(self, input, target):
+        return hybrid_loss(target, F.cross_entropy(input, target, reduction='none', weight=self.weight), self.gamma)
 
 class LDAMLoss(nn.Module):
     
